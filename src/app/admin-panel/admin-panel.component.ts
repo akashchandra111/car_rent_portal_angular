9025d83f63d71d349b37938183483ac3f79d991e
Dheeraj Joshi <djoshi19@in.ibm.com>
2019-07-09 14:59:27 +0530

    implemented session

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -11,23 +11,4 @@ export class AdminPanelComponent implements OnInit {
 
   ngOnInit() {
   }
-
-
-  fileData: File = null;
-  constructor(private http: HttpClient) { }
-
-  fileProgress(fileInput: any) {
-      this.fileData = <File>fileInput.target.files[0];
-  }
-
-  onSubmit() {
-      const formData = new FormData();
-      formData.append('file', this.fileData);
-      this.http.post('url/to/your/api', formData)
-        .subscribe(res => {
-          console.log(res);
-          alert('SUCCESS !!');
-        })
-  }
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { User } from './Interfaces/User';
 import { FetchJSONService } from './fetch-json.service';
+import { Router,NavigationEnd } from '@angular/router';
+import {HeaderComponent} from './header/header.component'
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'RentOCar';
-  loggedUser: User;
+ 
 
-  constructor(private carData: FetchJSONService)	{
+  constructor(private carData: FetchJSONService,private router: Router)	{
+
+       }
+
+
+  ngOnInit()	{
+
   }
 
-  ngOnInit($event)	{
-	  let data = sessionStorage.getItem('user');
-  	this.loggedUser = this.getUser(data);
-  }
+ getUser(){
 
- getUser(event){
-	console.log("here comes the user");
-
-	return event;
 }
 
 }
