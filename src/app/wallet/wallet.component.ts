a9438621d45282410a8ec79700b1814574750ce8
Gadiparthi Vyshnavi <gvyshn19@in.ibm.com>
2019-07-22 10:37:16 +0530

    Revert "added and completed wallet component"
    This reverts commit aca5ead92a775c868fafe368bb51baeb457513fc.

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,11 +1,4 @@
-// [Author - Vyshnavi]
-
 import { Component, OnInit } from '@angular/core';
-import { User } from '../Interfaces/User';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { FetchJSONService } from '../fetch-json.service';
-import { Login } from '../Interfaces/Login';
-import { Message } from 'src/app/Interfaces/Message';
 
 @Component({
   selector: 'app-wallet',
@@ -14,58 +7,9 @@ import { Message } from 'src/app/Interfaces/Message';
 })
 export class WalletComponent implements OnInit {
 
-  user: User;
-  toastMessage: string;
-  login: Login;
-  message: Message;
-  response:any;
-  moneyCard: number;
-  moneyNet: number;
-
-  constructor(private route: ActivatedRoute, private router: Router, private http: FetchJSONService) {
-    this.login= {
-		  id: JSON.parse(localStorage.getItem('user')).userId,
-		  password: JSON.parse(localStorage.getItem('user')).password
-    }
-    this.http.getUser(this.login).subscribe(
-      (data)=>  {
-        this.user = data['body'];
-    });    
-   }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  addMoney()  {
-    this.http.addMoney(this.user, this.moneyCard).subscribe(
-      (data: Message)=> {
-        if(data.status == "success")  {
-          this.toastMessage = "Money added successfully";
-        }
-        else  {
-          this.toastMessage = "Money not added";
-        }
-      }
-    );
-  }
-
-  addMoneyNet()  {
-    this.http.addMoney(this.user, this.moneyNet).subscribe(
-      (data: Message)=> {
-        if(data.status == "success")  {
-          this.toastMessage = "Money added successfully";
-        }
-        else  {
-          this.toastMessage = "Money not added";
-        }
-      }
-    );
-  }
-
-
-  // addMoney()  {
-  //   this.user.wallet = this.user.wallet + this.moneyCard;
-  //   console.log(this.user);
-  //   // this.http.updateUser
-  // }
 }
